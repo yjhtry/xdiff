f6f4ef36edd54ea9729e1617263602c742854b4c
yjh <860622588@qq.com>
2023-12-27 21:13:25 +0800

    feat: init xreq

--- src/bin/xdiff.rs
+++ src/bin/xdiff.rs
@@ -1,13 +1,49 @@
 use std::io::{stdout, Write};
 
-use anyhow::{anyhow, Result};
 use clap::Parser;
 use dialoguer::{theme::ColorfulTheme, Input, MultiSelect};
 use xdiff::{
-    cli::{Action, Args, RunArgs},
+    cli::{parse_key_val, KeyVal},
     utils::highlight,
     DiffConfig, DiffProfile, ExtraArgs, RequestProfile,
 };
+
+use anyhow::{anyhow, Result};
+use clap::Subcommand;
+
+/// Diff two HTTP requests and compare the differences of their responses.
+#[derive(Parser, Debug, Clone)]
+#[clap(version, author, about, long_about)]
+pub struct Args {
+    #[clap(subcommand)]
+    pub action: Action,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Action {
+    Run(RunArgs),
+    Parse(ParseArgs),
+}
+
+#[derive(Parser, Debug, Clone)]
+pub struct ParseArgs {}
+
+#[derive(Parser, Debug, Clone)]
+pub struct RunArgs {
+    /// profile name
+    #[clap(short, long, value_parser)]
+    pub profile: String,
+
+    /// Override args. Could be used to voerride the query, headers and body of the request.
+    /// for query params, use `-e key=value`
+    /// for headers, use `-e %key=value`
+    /// for body, use `-e @key=value`
+    #[clap(short, long, value_parser = parse_key_val, number_of_values=1)]
+    pub extra_params: Vec<KeyVal>,
+    /// config file
+    #[clap(short, long, value_parser)]
+    pub config: Option<String>,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -16,7 +52,6 @@ async fn main() -> Result<()> {
     match args.action {
         Action::Run(args) => run(args).await?,
         Action::Parse(_) => parse().await?,
-        _ => panic!("Unsupported action"),
     }
 
     Ok(())
--- src/cli.rs
+++ src/cli.rs
@@ -1,42 +1,6 @@
 use anyhow::{anyhow, Result};
-use clap::{Parser, Subcommand};
 
 use crate::ExtraArgs;
-
-/// Diff two HTTP requests and compare the differences of their responses.
-#[derive(Parser, Debug, Clone)]
-#[clap(version, author, about, long_about)]
-pub struct Args {
-    #[clap(subcommand)]
-    pub action: Action,
-}
-
-#[derive(Subcommand, Debug, Clone)]
-#[non_exhaustive]
-pub enum Action {
-    Run(RunArgs),
-    Parse(ParseArgs),
-}
-
-#[derive(Parser, Debug, Clone)]
-pub struct ParseArgs {}
-
-#[derive(Parser, Debug, Clone)]
-pub struct RunArgs {
-    /// profile name
-    #[clap(short, long, value_parser)]
-    pub profile: String,
-
-    /// Override args. Could be used to voerride the query, headers and body of the request.
-    /// for query params, use `-e key=value`
-    /// for headers, use `-e %key=value`
-    /// for body, use `-e @key=value`
-    #[clap(short, long, value_parser = parse_key_val, number_of_values=1)]
-    pub extra_params: Vec<KeyVal>,
-    /// config file
-    #[clap(short, long, value_parser)]
-    pub config: Option<String>,
-}
 
 /// key value pair
 #[derive(Debug, Clone)]
